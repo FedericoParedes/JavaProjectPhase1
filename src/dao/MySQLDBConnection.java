ee9fd8c5eb0cd31eba2c2fc39daa20925e804513
Federico Paredes <fede_paredes21@hotmail.com>
2024-02-18 23:42:55 -0300

    The MySQLDBConnection interface is created. This interface has the configuration that allows establishing the connection to the database

---
+++ src/dao/MySQLDBConnection.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public interface MySQLDBConnection {
+
+	default Connection getConnection(){
+		
+		
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		String URL = "jdbc:mysql://localhost:3306/peliculas";
+		
+		Connection conexion = null;
+		
+		try {
+			Class.forName(driver);
+			conexion = DriverManager.getConnection(URL, "root", "030818");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+				
+		 return conexion;
+			
+			
+		}	
+	
+	
+}
